e8d6b9eb5fdabed2193b02416b1c3e7e50b6d8ae
Seba Centurion <sebastiancenturion10@gmail.com>
2024-06-15 13:29:44 -0400

    initial commit

--- banco/graphql/models.py
+++ banco/graphql/models.py
@@ -1,7 +1,7 @@
-# models.py
-from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine, UniqueConstraint
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm.exc import NoResultFound
 
 DATABASE_URL = "sqlite:///../banco_gql.db"
 
@@ -10,28 +10,29 @@ Base = declarative_base()
 class Cliente(Base):
     __tablename__ = "clientes"
     id = Column(Integer, primary_key=True, index=True)
-    cedula = Column(String, unique=True, index=True)
-    nombre = Column(String, index=True)
-    apellido = Column(String, index=True)
+    cedula = Column(String, index=True, nullable=False)
+    nombre = Column(String, nullable=False)
+    apellido = Column(String, nullable=False)
     cuentas = relationship("Cuenta", back_populates="cliente")
 
 class Cuenta(Base):
     __tablename__ = "cuentas"
-    id = Column(Integer, primary_key=True, index=True)
-    cliente_id = Column(Integer, ForeignKey('clientes.id'))
-    cuenta = Column(Integer, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    cliente_id = Column(Integer, ForeignKey('clientes.id'), nullable=False)
+    cuenta = Column(Integer, unique=True, nullable=False)
     pagos = relationship("Pagos", back_populates="cuenta")
     cliente = relationship("Cliente", back_populates="cuentas")
 
 class Pagos(Base):
     __tablename__ = "pagos"
-    id = Column(Integer, primary_key=True, index=True)
-    cuenta_id = Column(Integer, ForeignKey('cuentas.id'))
-    monto = Column(Integer)
-    moneda = Column(String)
-    numero_factura = Column(String)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    cuenta_id = Column(Integer, ForeignKey('cuentas.id'), nullable=False)
+    monto = Column(Integer, nullable=False)
+    moneda = Column(String, nullable=False)
+    numero_factura = Column(String, nullable=False, unique=True)
     cuenta = relationship("Cuenta", back_populates="pagos")
 
+    __table_args__ = (UniqueConstraint('numero_factura', name='_numero_factura_uc'),)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- banco/graphql/schema.py
+++ banco/graphql/schema.py
@@ -1,10 +1,10 @@
 import strawberry
-import typing
-from typing import List, Optional, Union
+from typing import List, Optional
 from strawberry.types import Info
 from models import Cliente as ClienteModel, Cuenta as CuentaModel, Pagos as PagosModel, SessionLocal
-from sqlalchemy.orm import Session
 from sqlalchemy.orm import joinedload
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import IntegrityError
 
 
 @strawberry.type
@@ -21,6 +21,7 @@ class Pago:
     monto: int
     moneda: str
     numero_factura: str
+    cuenta: Cuenta
 
 @strawberry.type
 class Cliente:
@@ -33,18 +34,12 @@ class Cliente:
 @strawberry.type
 class Query:
 
-    # @strawberry.field
-    # def all_clientes(self, info: Info) -> List[Cliente]:
-    #     session = SessionLocal()
-    #     clientes = session.query(ClienteModel).all()
-    #     return clientes
-
     @strawberry.field
     def all_clientes(self, info: Info, nombre: Optional[str] = None) -> List[Cliente]:
         session = SessionLocal()
-        clientes = session.query(ClienteModel).options(
-                joinedload(ClienteModel.cuentas).joinedload(CuentaModel.pagos)
-            ).all()
+        query = session.query(ClienteModel).options(
+            joinedload(ClienteModel.cuentas).joinedload(CuentaModel.pagos)
+        )
         if nombre:
             query = query.filter(ClienteModel.nombre == nombre)
         clientes = query.all()
@@ -52,8 +47,11 @@ class Query:
     
     @strawberry.field
     def pagos_por_cuenta(self, info: Info, cuenta: int) -> List[Pago]:
-       '''A IMPLEMENTAR'''
-       return None
+        session = SessionLocal()
+        pagos = session.query(PagosModel).options(
+            joinedload(PagosModel.cuenta)
+        ).filter(PagosModel.cuenta_id == cuenta).all()
+        return pagos
 
     @strawberry.field
     def all_cuentas(self, info: Info) -> List[Cuenta]:
@@ -82,20 +80,35 @@ class Mutation:
     def create_cuenta(self, info: Info, cliente_id: int, cuenta: int) -> Cuenta:
         session = SessionLocal()
         cuenta = CuentaModel(cliente_id=cliente_id, cuenta=cuenta)
-        session.add(cuenta)
-        session.commit()
-        session.refresh(cuenta)
+        try:
+            session.add(cuenta)
+            session.commit()
+            session.refresh(cuenta)
+        except IntegrityError:
+            session.rollback()
+            raise ValueError("El número de cuenta debe ser único y requerido.")
         return cuenta
 
     @strawberry.mutation
-    def create_pago(self, info: Info,cuenta_id: int, monto: int, moneda: str, numero_factura: str) -> Pago:
+    def create_pago(self, info: Info, cuenta_id: int, monto: int, moneda: str, numero_factura: str) -> Pago:
         session = SessionLocal()
-        pago = PagosModel(cuenta_id=cuenta_id, monto=monto, moneda=moneda, numero_factura=numero_factura)
-        session.add(pago)
-        session.commit()
-        session.refresh(pago)
+        try:
+            # Validar existencia de cuenta y cliente
+            cuenta = session.query(CuentaModel).filter(CuentaModel.id == cuenta_id).one()
+            cliente = session.query(ClienteModel).filter(ClienteModel.id == cuenta.cliente_id).one()
+            
+            # Crear pago
+            pago = PagosModel(cuenta_id=cuenta_id, monto=monto, moneda=moneda, numero_factura=numero_factura)
+            session.add(pago)
+            session.commit()
+            session.refresh(pago)
+            session.refresh(pago.cuenta)
+        except NoResultFound:
+            session.rollback()
+            raise ValueError("La cuenta o cliente no existen o no coinciden.")
+        except IntegrityError:
+            session.rollback()
+            raise ValueError("No se puede pagar más de una vez una misma factura. Todos los datos son requeridos.")
         return pago
     
-    
-
 schema = strawberry.Schema(query=Query, mutation=Mutation)
